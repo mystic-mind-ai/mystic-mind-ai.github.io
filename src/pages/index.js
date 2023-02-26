9a5cb2145cd2e1e326fc3636d361d782920b9fe1
frostplanet9 <98440455+frostplanet9@users.noreply.github.com>
2023-02-26 13:06:05 -0500

    Refactoring pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import * as React from "react"
-import "../styles/index.css"
+import * as indexStyles from "../styles/index.module.css"
 import 'animate.css';
-import logo from "../images/logo.svg"
 
 import Trails from "../components/trails"
 
@@ -16,23 +15,21 @@ const IndexPage = () => {
           </nav>
         </div>
         <div>
-          <h1 className="animate__animated animate__fadeInDown logoStyle">Mystic Mind</h1>
-          <p className="animate__animated animate__fadeIn animate__delay-1s tagline">The premier AI solution for generating RPG content</p>
-          <a className="discordButton" href="https://discord.gg/gpFPdVxuYq" target="_blank" rel="noreferrer">Join us on Discord</a>
+          <h1 className={`animate__animated animate__fadeInDown ${indexStyles.fadeInDown} ${indexStyles.logoStyle}`}>Mystic Mind</h1>
+          <p className={`animate__animated animate__fadeIn animate__delay-1s ${indexStyles.fadeIn} ${indexStyles.tagline}`}>The premier AI solution for generating RPG content</p>
+          <a className={indexStyles.discordButton} href="https://discord.gg/gpFPdVxuYq" target="_blank" rel="noreferrer">Join us on Discord</a>
         </div>
         <div />
       </div>
 
-      <img className="bkgLogo" src={logo} alt="Mystic Mind logo" />
-
       <h2 id="about">About</h2>
       <p>Mystic Mind is the premier AI solution for generating any RPG content you wish. Create spells, items, monsters, and more with a single command.</p>
       <p>We are a small team that is focused on delivering unique solutions for the RPG community.</p>
-      <div className="sectionSpacer"></div>
+      <div className={indexStyles.sectionSpacer}></div>
 
       <h2 id="support">Support</h2>
       <p>For help, questions, or inquiries, please <a href="https://discord.gg/gpFPdVxuYq" target="_blank" rel="noreferrer">visit our Discord</a> for more information.</p>
-      <div className="sectionSpacer"></div>
+      <div className={indexStyles.sectionSpacer}></div>
 
       <Trails />
     </main>
@@ -43,6 +40,7 @@ export default IndexPage
 
 export const Head = () => (
   <>
+    <html lang="en" className={indexStyles.bkgLogo} />
     <title>Mystic Mind</title>
     <meta name="description" content="Mystic Mind is the premier AI solution for RPG content." />
   </>
