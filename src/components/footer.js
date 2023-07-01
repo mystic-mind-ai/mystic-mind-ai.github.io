12792629477bb2a98e3ec14536b96379c9f157a0
frostplanet9 <98440455+frostplanet9@users.noreply.github.com>
2023-07-01 19:04:38 -0400

    Adding terms and footer (#1)
    * Adding terms and footer
    
    * Terms updates and styling
    
    * Minor package.json change

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+function Footer() {
+  return (
+      <p className="footer">
+          © 2023 Invictus Technologies LLC 🞄 <Link to="/terms">Terms</Link>
+      </p>
+    )
+}
+
+export default Footer
+
